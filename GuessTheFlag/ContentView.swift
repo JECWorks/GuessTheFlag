fbaf9a4e4c62f3d253eb18f7e153367a4458969c
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-08-06 22:15:18 -0400

    Buttons and images

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    func executeDelete() {
+        print("Now deleting")
+    }
     var body: some View {
-        Text("Your content")
+        VStack {
+            Text("Your content")
+            Button("Delete selection", role: .destructive, action: executeDelete)
+                .buttonStyle(.bordered)
+            Button("Button 2") {
+                print("Button 2 was tapped")
+            }
+            .buttonStyle(.borderedProminent)
+            Button {
+                print("Button3 tapped")
+            } label: {
+                Image(systemName: "pencil.circle")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.blue)
+            }
+            Button("Edit", systemImage: "pencil") {
+                print("Edit button tapped")
+            }
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Label("Edit", systemImage: "pencil")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.blue)
+            }
+        }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .foregroundColor(.white)
             .background(.teal.gradient)
