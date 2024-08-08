fcb586e17b215022871f1f8f62aba98104ae0c49
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-08-07 22:06:19 -0400

    Basic UI for GuessTheFlag and added flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,51 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+
 
     func executeDelete() {
         print("Now deleting")
     }
     var body: some View {
-        VStack {
-            Text("Your content")
-            Button("Delete selection", role: .destructive, action: executeDelete)
-                .buttonStyle(.bordered)
-            Button("Button 2") {
-                print("Button 2 was tapped")
-            }
-            .buttonStyle(.borderedProminent)
-            Button {
-                print("Button3 tapped")
-            } label: {
-                Image(systemName: "pencil.circle")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.blue)
-            }
-            Button("Edit", systemImage: "pencil") {
-                print("Edit button tapped")
-            }
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Label("Edit", systemImage: "pencil")
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.blue)
-            }
-            Button("Show Alert") {
-                        showingAlert = true
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        // flag was tapped
+                    } label: {
+                        Image(countries[number])
                     }
-                    .alert("Important message", isPresented: $showingAlert) {
-                        Button("Delete", role: .destructive) { }
-                        Button("Cancel", role: .cancel) { }
-                    } message: {
-                        Text("Please read this.")
-                    }
+                }
+            }
         }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .foregroundColor(.white)
-            .background(.teal.gradient)
     }
 }
 
