53b4f5cb29cc654ccc1ff0c87ce7aa73c44fdfd2
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-10-17 21:32:17 -0400

    Animation challenge

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var score = 0
     @State private var tries = 0
+    @State private var rotationAmount = 0.0
+    @State private var selectedFlag: Int? = nil
     
     let maxTries = 8
 
@@ -51,6 +53,13 @@ struct ContentView: View {
                             Image(countries[number])
                                 .clipShape(.capsule)
                                 .shadow(radius: 5)
+                                .rotation3DEffect(
+                                    .degrees(number == correctAnswer ? rotationAmount : 0),
+                                    axis: (x: 0, y: 1, z: 0)
+                                )
+                                .opacity(selectedFlag == nil || selectedFlag == number || number == correctAnswer ? 1 : 0.25)  // Fade out incorrect flags
+
+                                .animation(.easeInOut(duration: 1.0), value: rotationAmount)
                         }
                     }
                     
@@ -87,11 +96,13 @@ struct ContentView: View {
         
     }
     func flagTapped(_ number: Int) {
+        selectedFlag = number
         if tries < maxTries {
             if number == correctAnswer {
                 scoreTitle = "Correct"
                 score += 1
                 tries += 1
+                rotationAmount += 360
             } else {
                 scoreTitle = "Wrong, choice! That's the flag of \(countries[number])"
                 tries += 1
@@ -109,14 +120,18 @@ struct ContentView: View {
     func askQuestion () {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        rotationAmount = 0
+        selectedFlag = nil
         
     }
     
     func resetGame() {
     score = 0
     tries = 0
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
+    rotationAmount = 0
+    selectedFlag = nil
     }
 }
 
